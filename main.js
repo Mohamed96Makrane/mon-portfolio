542b81b9295188a68a18c5464a278b0e634330f2
MAKRAN Mohamed <med96.makrane@gmail.com>
2025-12-05 15:54:35 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.text', {
+    strings: ["JAVA Developer", "JAVA-SCRIPT Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
